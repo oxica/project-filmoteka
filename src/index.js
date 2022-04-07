b9f82447335b7a11d4a82079d9fd82d93e7fe43d
Oksana <kskaplun@gmail.com>
2022-04-07 17:40:58 +0300

    add settings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './sass/main.scss';
+
+import './js/refs';
+import './js/teamModal';
+import './js/switchDayNight';
+import './js/apiSevice';
+import './js/backToTop';
+import './js/filmModal';
+import './js/galleryFetch';
+import './js/headerBtn';
+import './js/localeStorageServices';
+import './js/pagination';
+import './js/search';
