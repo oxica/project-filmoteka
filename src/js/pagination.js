88e8c457d6fb911e64d11c004cb1c80ff12e03ae
Irina Kabaliuk <i.kabaliuk@weplay.tv>
2022-04-08 22:28:06 +0300

    paignation

---
+++ src/js/pagination.js
@@ -0,0 +1,32 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css'
+
+const container = document.getElementById('pagination');
+
+const options = { // below default value of options
+     totalItems: 10,
+     itemsPerPage: 10,
+     visiblePages: 10,
+     page: 1,
+     centerAlign: false,
+     firstItemClassName: 'tui-first-child',
+     lastItemClassName: 'tui-last-child',
+     template: {
+         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+         moveButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</a>',
+         disabledMoveButton:
+             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</span>',
+         moreButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+                 '<span class="tui-ico-ellip">...</span>' +
+             '</a>'
+     }
+};
+
+new Pagination(container, options);
