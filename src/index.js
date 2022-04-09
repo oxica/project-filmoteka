f3a38a9deca0399e99159ae3ca21a6c8145b33dd
Oksana <kskaplun@gmail.com>
2022-04-09 10:36:09 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './sass/main.scss';
 import './js/refs';
 import './js/teamModal';
 import './js/switchDayNight';
-// import './js/apiSevice';
+import './js/apiSevice';
 import './js/backToTop';
 import './js/filmModal';
 import './js/galleryFetch';
