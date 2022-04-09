b9434c2d26c776eae4b3fb1659cfa48ad37141d8
Kristina <ktuzova1901@gmail.com>
2022-04-09 18:46:03 +0300

    added coding

--- src/js/localization.js
+++ src/js/localization.js
@@ -22,3 +22,11 @@ const allLang = ['en', 'ua']
 //     select.value = hash;
 // }
 // changeLanguage()
+// const select = document.querySelector('select');
+// select.addEventListener('change', changeURLLanguage)
+// function changeURLLanguage() {
+//     let lang = select.value;
+//     location.href = window.location.pathname + '#' + lang;
+//     location.reload
+// }
+
