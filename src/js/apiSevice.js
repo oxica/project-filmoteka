e73a115fd5d4ce4234390c12b28de4c4292cd638
YaroslavPlaksii91 <yarosyk@ukr.net>
2022-04-10 15:42:41 +0300

    gallery

--- src/js/apiSevice.js
+++ src/js/apiSevice.js
@@ -1,72 +1,76 @@
 import axios from 'axios';
-const API_KEY = "7a92417a5af1e8667d171d8c5ef3af4e";
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
+const API_KEY = '7a92417a5af1e8667d171d8c5ef3af4e';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
-
-
-export class API_service{
+export class API_service {
   language = localStorage.getItem('language');
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-        this.language;
-    }
- 
-  
-    async fetchTrending() {
-      try {
-      const { data } = await axios("trending/all/day", {
-            params: {
-            api_key: API_KEY 
-            }});
-          this.page++;
-          console.log(data);
-      return data;
-      } catch (error) {
-      console.log(error);
-      }
-    }
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.language;
+  }
 
-    async fetchMoviesByKeyword() {
-      try {
-      const { data } = await axios("search/movie", {
-            params: {
-            api_key: API_KEY,
-            query: this.searchQuery,
-            language: this.language,
-            page:this.page,
-            }});
-          this.page++;
-          console.log(data);
-      return data //returns an OBJECT. e.g.{page: 1, results: Array(20), total_pages: 8, total_results: 147}
-      } catch (error) {
-      console.log(error)
-      }
-     }
-  
-  
-    async fetchMovieById() {//will throw an error if title "undefined";
-      try {
-      const { data }  = await axios(`movie/${this.searchQuery}` , { //for this to work make sure this.searchQuery type is number!!! 
-            params: {
-            api_key: API_KEY,
-            language: this.language,
-            }});
-      console.log(data)
-      return data
+  async fetchTrending() {
+    try {
+      Loading.pulse();
+      const { data } = await axios('trending/all/day', {
+        params: {
+          api_key: API_KEY,
+        },
+      });
+      this.page++;
+      Loading.remove();
+      console.log(data.results);
+      return data.results;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
+  }
 
+  async fetchMoviesByKeyword() {
+    try {
+      Loading.pulse();
+      const { data } = await axios('search/movie', {
+        params: {
+          api_key: API_KEY,
+          query: this.searchQuery,
+          language: this.language,
+          page: this.page,
+        },
+      });
+      this.page++;
+      Loading.remove();
+      console.log(data.results);
+      return data.results; //returns an OBJECT. e.g.{page: 1, results: Array(20), total_pages: 8, total_results: 147}
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async fetchMovieById() {
+    //will throw an error if title "undefined";
+    try {
+      const { data } = await axios(`movie/${this.searchQuery}`, {
+        //for this to work make sure this.searchQuery type is number!!!
+        params: {
+          api_key: API_KEY,
+          language: this.language,
+        },
+      });
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
-
-    get query() {
-      return this.searchQuery;
-    }
-
-    set query(newQuery) {
-      this.searchQuery = newQuery;
-    }
-}
-
