b1a51158507dfddc3b8e26c1469a590d5edc1a0b
Kristina <ktuzova1901@gmail.com>
2022-04-16 22:05:34 +0300

    styles

--- src/js/choices.js
+++ src/js/choices.js
@@ -1,7 +1,10 @@
 import  Choices from 'choices.js';
 let selectStateInputEl = document.querySelector('.header__select');
 const choices = new Choices(selectStateInputEl, {
-  searchEnabled:false,
+  searchEnabled: false,
+  itemSelectText: ""
 })
 let selectGenre = document.querySelector('.genre');
-const choicesgenre = new Choices(selectGenre)
+const choicesgenre = new Choices(selectGenre, {
+   itemSelectText: ""
+})
