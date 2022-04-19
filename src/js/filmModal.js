75c6b652bbae9dc93ffbc3b722e3293af5d78477
Kristina <ktuzova1901@gmail.com>
2022-04-19 17:17:53 +0300

    fixing

--- src/js/filmModal.js
+++ src/js/filmModal.js
@@ -360,7 +360,12 @@ function onQueueModalBtnClick(e) {
         }
       });
     }
-    queueModalBtn.textContent = 'Remove';
+    if (lng === 'en') {
+      queueModalBtn.textContent = 'Remove';
+    }
+    else {
+      queueModalBtn.textContent = 'Видаляти';
+    }
   }
   queueModalBtn.classList.toggle('active');
 }
